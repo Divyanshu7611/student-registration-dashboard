d5ede446d47e517a2e516db27101dd67b02ef0e8
divyanshu <dshapbss202@gmail.com>
2025-03-02 12:33:47 +0530

    token verification

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { verifyToken } from './lib/auth';
+import { Console } from 'console';
+import Jwt from 'jsonwebtoken';
 
 export function middleware(request: NextRequest) {
   // Get the pathname
@@ -11,19 +13,24 @@ export function middleware(request: NextRequest) {
 
   // Get the token from cookies
   const token = request.cookies.get('auth-token')?.value;
+  // console.log(token)
 
-  // If it's an admin route and no token or invalid token, redirect to login
+
   if (isAdminRoute) {
     if (!token) {
       return NextResponse.redirect(new URL('/login', request.url));
     }
 
     try {
-      const decoded = verifyToken(token);
+      const decoded = Jwt.decode(token);
+      // console.log("dedocde token",Jwt.decode(token))
       if (!decoded || (typeof decoded !== 'object' || decoded.role !== 'admin')) {
+        // console.log("There might be error",decoded)
+
         return NextResponse.redirect(new URL('/login', request.url));
       }
     } catch (error) {
+      // console.log("There might be error",error)
       return NextResponse.redirect(new URL('/login', request.url));
     }
   }
